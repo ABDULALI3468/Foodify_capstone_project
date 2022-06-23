f70260509656312ab3e8c94da2654e5796c2a7a4
ABDULALI3468 <mrabdulali2003@gmail.com>
2022-06-23 09:39:29 -0700

    Solved the linters error.

--- src/likes.js
+++ src/likes.js
@@ -1,7 +1,7 @@
-const appId = "1AkDSb9CeEmOpdltysrI";
+const appId = '1AkDSb9CeEmOpdltysrI';
 export const likeFetcher = async () => {
-  const cards = document.querySelectorAll(".card");
-  const likeCount = document.querySelectorAll(".like-count");
+  const cards = document.querySelectorAll('.card');
+  const likeCount = document.querySelectorAll('.like-count');
   await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appId}/likes`)
     .then((response) => response.json())
     .then((json) => {
@@ -16,17 +16,17 @@ export const likeFetcher = async () => {
 };
 
 export const likeCreator = async () => {
-  const heartIcon = document.querySelectorAll(".fa-heart");
+  const heartIcon = document.querySelectorAll('.fa-heart');
   heartIcon.forEach((elem) => {
-    elem.addEventListener("click", (e) => {
-      e.target.classList.remove("fa-regular");
-      e.target.classList.add("fa-solid");
+    elem.addEventListener('click', (e) => {
+      e.target.classList.remove('fa-regular');
+      e.target.classList.add('fa-solid');
       e.target.nextSibling.innerHTML = Number(e.target.nextSibling.innerHTML) + 1;
-      fetch("https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/1AkDSb9CeEmOpdltysrI/likes", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
+      fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/1AkDSb9CeEmOpdltysrI/likes', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
-          item_id: e.target.getAttribute("target_id"),
+          item_id: e.target.getAttribute('target_id'),
         }),
       });
     });

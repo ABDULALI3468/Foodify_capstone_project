e1334d81420cf8b5766058d326baa1e2340079e9
ABDULALI3468 <mrabdulali2003@gmail.com>
2022-06-23 04:39:52 -0700

    Did the required changes.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-// import onready from "./app.js";
-import './assets/styles.css';
-import './assets/popup.css';
-import displayCards from './displayCards.js';
+import "./assets/styles.css";
+import "./assets/popup.css";
+import displayCards from "./displayCards.js";
 
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener("DOMContentLoaded", () => {
   displayCards();
-  // onready();
 });

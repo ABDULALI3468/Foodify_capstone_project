ca961a765f9f2dbe6cf93aea513deb4ace9a2d2f
ABDULALI3468 <mrabdulali2003@gmail.com>
2022-06-23 04:15:06 -0700

    Added the feature of showcasing recipie cards.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-import onready from './app.js';
+// import onready from "./app.js";
 import './assets/styles.css';
 import './assets/popup.css';
+import displayCards from './displayCards.js';
 
-window.addEventListener('DOMContentLoaded', onready);
+window.addEventListener('DOMContentLoaded', () => {
+  displayCards();
+  // onready();
+});
